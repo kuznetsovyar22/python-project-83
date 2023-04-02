49e2aa5263ee81f5690d72e97a78b3cc01993a9e
kuznetsovyar22 <kuznetsovyar@mail.ru>
2023-04-03 00:28:24 +0300

    4th

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -38,7 +38,10 @@ def index():
 def get_urls():
     cur.execute("SELECT * FROM urls")
     records = cur.fetchall()[::-1]
-    return render_template('urls.html', urls=records)
+    cur.execute("SELECT DISTINCT ON (url_id) url_id, created_at FROM url_checks ORDER BY url_id, created_at DESC")
+    lastcheck = cur.fetchall()
+    print(lastcheck)
+    return render_template('urls.html', urls=records, lastcheck=lastcheck)
 
 
 @app.post('/urls')
@@ -46,22 +49,25 @@ def add_url():
     newurl = request.form.get('url')
     if url(newurl) and len(newurl) < 255:
         query = "INSERT INTO urls (name, created_at) VALUES (%s, %s)"
-        cur.execute(query, (newurl, datetime.date.today()))
+        cur.execute(query, (newurl, datetime.datetime.now()))
+        cur.execute('SELECT id FROM urls ORDER BY id DESC')
+        id = cur.fetchall()[0]
         flash('Страница успешно добавлена', 'success')
-        return redirect('/urls/{id}', 302)
+        return redirect(url_for('cur_url', id=id[0]), 302)
     else:
         if len(newurl) == 0:
             flash('URL обязателен', 'warning')
         flash('Некорректный URL', 'warning')
-        return redirect('/', 402)
+        return redirect(url_for('index'), 402)
 
 
 
 @app.route('/urls/<id>')
 def cur_url(id):
-    cur.execute("SELECT * FROM urls ORDER BY id DESC LIMIT 1")
+    cur.execute(f"SELECT * FROM urls WHERE id={id} ORDER BY id DESC")
     current_url = cur.fetchall()[0]
-    cur.execute("SELECT * FROM url_checks ORDER BY id DESC")
+    
+    cur.execute(f"SELECT * FROM url_checks WHERE url_id={id} ORDER BY id DESC")
     allchecks = cur.fetchall()
     messages = get_flashed_messages(with_categories=True)
     return render_template('cururl.html', messages=messages, current_url=current_url, allchecks=allchecks)
@@ -69,7 +75,7 @@ def cur_url(id):
 
 @app.post('/urls/<id>/checks')
 def check_url(id):
-    query = "INSERT INTO url_checks (url_id, status_code, h1, title, created_at) VALUES (%s, %s, %s, %s, %s)"
-    cur.execute(query, (id, 200, 'h1', 'title', datetime.date.today()))
+    query = "INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
+    cur.execute(query, (id, 200, 'h1', 'title', 'description', datetime.datetime.now()))
     flash('Страница успешно проверена', 'success')
     return redirect(url_for('cur_url', id=id), 302)
